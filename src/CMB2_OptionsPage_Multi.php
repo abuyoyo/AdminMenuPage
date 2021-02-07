a380a523c123c566bf528f51dd3a3c4a260632a7
abuyoyo <abuyoyo@users.noreply.github.com>
2021-02-08 01:19:07 +0200

    Fix CMB2_OptionsPage_Multi to use cmb2_override_fields

--- src/CMB2_OptionsPage_Multi.php
+++ src/CMB2_OptionsPage_Multi.php
@@ -17,5 +17,12 @@ include_once __DIR__ . '/CMB2_Override_Meta.php';
 if ( ! class_exists( 'WPHelper\CMB2_OptionsPage_Multi' ) ):
 class CMB2_OptionsPage_Multi extends CMB2_OptionsPage{
 	use CMB2_Override_Meta;
+
+	function __construct( $admin_page )
+	{
+		parent::__construct( $admin_page );
+
+		$this->cmb2_override_fields( $this->fields );
+	}
 }
 endif;
