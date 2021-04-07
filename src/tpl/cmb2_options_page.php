17b9cb0a152170f77dc5e4f35c58a17644189598
abuyoyo <abuyoyo@users.noreply.github.com>
2021-04-08 01:04:31 +0300

    Use CMB2 ~2.9 options_page_tab_nav_output() to generate tabs
    see https://github.com/CMB2/CMB2/issues/1407
    props: @abuyoyo

--- src/tpl/cmb2_options_page.php
+++ src/tpl/cmb2_options_page.php
@@ -5,22 +5,17 @@
  * @var WPHelper\CMB2_Options_Page $this
  * @var CMB2_Options_Hook $hookup
  * @global array $tabs
- * @global string $hookup_static - class name of $this
  */
 
-$hookup_static = get_class($hookup);
 $tabs = $hookup->get_tab_group_tabs();
- ?>
+
+?>
  <div class="wrap cmb2-options-page option-<?php echo esc_attr( sanitize_html_class( $hookup->option_key ) ); ?>">
 	<?php if ( $this->cmb->prop( 'title' ) ) : ?>
 		<h2><?php echo wp_kses_post( $this->cmb->prop( 'title' ) ); ?></h2>
 	<?php endif; ?>
 	<?php if ( count( $tabs ) > 1 ) : ?>
-		<h2 class="nav-tab-wrapper">
-			<?php foreach ( $tabs as $option_key => $tab_title ) : ?>
-				<a class="nav-tab<?php if ( $hookup_static::is_page( $option_key ) ) : ?> nav-tab-active<?php endif; ?>" href="<?php menu_page_url( $option_key ); ?>"><?php echo wp_kses_post( $tab_title ); ?></a>
-			<?php endforeach; ?>
-		</h2>
+		<?php $hookup->options_page_tab_nav_output(); ?>
 	<?php endif; ?>
 	<form class="cmb-form" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="POST" id="<?php echo $this->cmb->cmb_id; ?>" enctype="multipart/form-data" encoding="multipart/form-data">
 		<input type="hidden" name="action" value="<?php echo esc_attr( $hookup->option_key ); ?>">
