1caed01ec8d2f990f3a4d26df2eda9cfef24da14
abuyoyo <abuyoyo@users.noreply.github.com>
2023-03-20 16:35:50 +0200

    tpl/sidebar-add-meta-boxes.php

--- src/tpl/wrap-sidebar.php
+++ src/tpl/wrap-sidebar.php
@@ -21,39 +21,7 @@
 
 			<!-- sidebar -->
 			<div id="postbox-container-1" class="postbox-container">
-				<style>
-					/*
-					we don't actually have draggable/movable metaboxes
-					.hide-if-no-js / hidden classes would help
-					 */
-					.handle-actions {
-						display: none;
-					}
-					.postbox .postbox-header .hndle {
-						cursor: unset;
-					}
-				</style>
-				<?php
-				/**
-				 * Allow meta-boxes to hook to this page ('side' context).
-				 * 
-				 * 
-				 */
-				do_action('add_meta_boxes', $this->get_hook_suffix(), 'side' );
-				
-				/**
-				 * Remove 'Featured Image' meta-box added by core.
-				 * 
-				 * @see register_and_do_post_meta_boxes() (wp-admin/includes/meta-boxes.php)
-				 * @todo Investigate why $thumbnail_support returns true for our pages.
-				 */ 
-				remove_meta_box( 'postimagediv', $this->get_hook_suffix(), 'side' );
-
-				/**
-				 * Render meta-boxes
-				 */
-				do_meta_boxes( $this->get_hook_suffix(), 'side', null );
-				?>
+				<?php include 'sidebar-add-meta-boxes.php'; ?>
 			</div><!-- #postbox-container-1 .postbox-container -->
 		</div><!-- #post-body -->
 		<div class="clear"></div>
